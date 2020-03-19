5b1f6ab3c8569c64679fa6ccaed8dbf270b855e1
Mark Stabler <mark@markstabler.com>
2020-03-19 22:10:14 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -2,7 +2,7 @@ class Vehicle
   attr_accessor :wheel_size, :wheel_number
 
   def initialize(wheel_size, wheel_number)
-    @wheel_size = wheel_size 
+    @wheel_size = wheel_size
     @wheel_number = wheel_number
   end
 
@@ -15,5 +15,5 @@ class Vehicle
   end
 
 
-  
+
 end
